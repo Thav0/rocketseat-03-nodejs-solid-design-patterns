4dc2a2c1cd0e3dcb3bd7c3ed53c2a7de51c2361b
Gustavo Diniz <gustavodnz035@gmail.com>
2025-09-08 20:25:28 -0300

    Updated migrations, docker compose added

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,4 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-prisma.user.create({
-  data: {
-    name: 'teste',
-    email: 'gustavodnz035@gmail.com',
-  },
-})
-
 export const app = fastify()
