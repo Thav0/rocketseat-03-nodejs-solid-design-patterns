ed3b133a544417d43512118a83b37a7a599ad87f
Gustavo Diniz <gustavodnz035@gmail.com>
2025-09-08 16:26:08 -0300

    Added prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'teste',
+    email: 'gustavodnz035@gmail.com',
+  },
+})
 
 export const app = fastify()
