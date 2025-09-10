547df404a614d712e3919b6670168d35cf139078
Gustavo Diniz <gustavodnz035@gmail.com>
2025-09-10 18:57:47 -0300

    Added new error type and authenticate use case

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,41 @@
+import { UsersRepository } from '@/repositories/prisma/users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
+import { compare } from 'bcryptjs'
+import { User } from '@prisma/client'
+
+interface AuthenticateUseCaseRequest {
+  email: string
+  password: string
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    // Check email
+    // Check if the database password matches with the informed
+
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new InvalidCredentialsError()
+    }
+
+    const doesPasswordMatches = await compare(password, user.password_hash)
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError()
+    }
+
+    return {
+      user,
+    }
+  }
+}
