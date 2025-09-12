e3123b375138bee08fef15079523de91449146a5
Gustavo Diniz <gustavodnz035@gmail.com>
2025-09-11 21:53:44 -0300

    Implemented Factory Pattern and added new use case to get user profile

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,16 @@
 import { prisma } from '@/lib/prisma'
-import { Prisma } from '@prisma/client'
+import { Prisma, User } from '@prisma/client'
 import { UsersRepository } from './users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(userId: string): Promise<User | null> {
+    return await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    })
+  }
+
   async findByEmail(email: string) {
     return await prisma.user.findUnique({
       where: {
